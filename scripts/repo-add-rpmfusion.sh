71e2af2513a3a3a031ad47b48162a0785115cb96
Faeiz Mahrus <faeizmahrus@outlook.com>
2025-03-05 23:25:28 +0600

    fix: set fedora version in script

--- scripts/repo-add-rpmfusion.sh
+++ scripts/repo-add-rpmfusion.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-rpm-ostree install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+FEDORAVER=41
+rpm-ostree install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-"$FEDORAVER".noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-"$FEDORAVER".noarch.rpm
 rpm-ostree update --uninstall rpmfusion-free-release --uninstall rpmfusion-nonfree-release --install rpmfusion-free-release --install rpmfusion-nonfree-release
 rpm-ostree install rpmfusion-free-release-tainted rpmfusion-nonfree-release-tainted
