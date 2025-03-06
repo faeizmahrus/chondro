9e7776df5bdd586b8005b70d72d3105360767fe1
Faeiz Mahrus <167124477+faeizmahrus@users.noreply.github.com>
2025-03-07 01:54:44 +0600

    Update repo-add-rpmfusion.sh

--- scripts/repo-add-rpmfusion.sh
+++ scripts/repo-add-rpmfusion.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-FEDORAVER=41
-rpm-ostree install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-"$FEDORAVER".noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-"$FEDORAVER".noarch.rpm
+rpm-ostree install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-41.noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-41.noarch.rpm
 rpm-ostree update --uninstall rpmfusion-free-release --uninstall rpmfusion-nonfree-release --install rpmfusion-free-release --install rpmfusion-nonfree-release
 rpm-ostree install rpmfusion-free-release-tainted rpmfusion-nonfree-release-tainted
